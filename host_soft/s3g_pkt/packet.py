bdb15d445bec33f8c97458480714737dc3b21403
Vsevolod Lobko <seva@sevik.org>
2018-12-22 11:43:58 +0200

    remove cruft

--- host_soft/s3g_pkt/packet.py
+++ host_soft/s3g_pkt/packet.py
@@ -20,71 +20,31 @@ def encode_payload(payload):
 
 def decode_packet(packet):
     """
-    Decode a packet from a payload.Non-streaming packet decoder.
-    Accepts a byte array containing a single packet, and attempts
-    to parse the packet and return the payload.
-    @param packet byte array containing the input packet
-    @return payload of the packet
+    Decode a packet from a payload.
+    Accepts a byte array containing a stream subset, and attempts
+    to parse the packet and return the payload and rest of stream.
+    @param packet byte array containing stream data
+    @return payload of the packet and rest of stream
     """
     assert type(packet) is bytearray
 
     if len(packet) < 4:
         raise ValueError("Packet too short: %d < 4" % len(packet))
 
-    if packet[0] != 0xD5:
-        raise ValueError("Wrong Header: %x != 0xD5" % (packet[0], ))
+    for junk_bytes in range(0, len(packet) - 3):
+        if packet[junk_bytes] != 0xD5:
+            continue
 
-    if packet[1] != len(packet) - 3:
-        raise ValueError("Length mismatch: %d != %d" % (packet[1], len(packet) - 3))
+        payload_len = packet[junk_bytes + 1]
+        if payload_len > len(packet) - 3 - junk_bytes:
+            raise ValueError("Packet too short")
 
-    crc = calculate_crc(packet[2:(len(packet) - 1)])
-    if packet[len(packet) - 1] != crc:
-        raise ValueError("CRC mismatch: %x != %x" % (packet[len(packet) - 1], crc))
-
-    return packet[2:(len(packet) - 1)]
-
-
-class PacketStreamDecoder(object):
-
-    """
-    A state machine that accepts bytes from an s3g packet stream, checks the validity of
-    each packet, then extracts and returns the payload.
-    """
-    def __init__(self):
-        """
-        Initialize the packet decoder
-        """
-        self.state = 'WAIT_FOR_HEADER'
-        self.payload = bytearray()
-        self.expected_length = 0
-
-    def parse_byte(self, byte):
-        """
-        Entry point, call for each byte added to the stream.
-        @param byte Byte to add to the stream
-        """
-
-        if self.state == 'WAIT_FOR_HEADER':
-            if byte != 0xD5:
-                raise ValueError("Header mismatch: %x != 0xD5" % (byte))
-
-            self.state = 'WAIT_FOR_LENGTH'
-
-        elif self.state == 'WAIT_FOR_LENGTH':
-            self.expected_length = byte
-            self.state = 'WAIT_FOR_DATA'
-
-        elif self.state == 'WAIT_FOR_DATA':
-            self.payload.append(byte)
-            if len(self.payload) == self.expected_length:
-                self.state = 'WAIT_FOR_CRC'
-
-        elif self.state == 'WAIT_FOR_CRC':
-            crc = calculate_crc(self.payload)
-            if crc != byte:
-                raise ValueError("CRC mismatch: %x != %x" % (crc, byte))
-
-            self.state = 'PAYLOAD_READY'
-
+        payload = packet[junk_bytes + 2:junk_bytes + 2 + payload_len]
+        crc = calculate_crc(payload)
+        packet_crc = packet[junk_bytes + payload_len + 2 ]
+        if packet_crc != crc:
+            raise ValueError("CRC mismatch: %x != %x" % (packet_crc, crc))
         else:
-            raise ValueError('Parser in bad state: too much data provided?')
+            return payload, packet[junk_bytes + payload_len + 3:]
+    raise ValueError("Start byte not found")
+
