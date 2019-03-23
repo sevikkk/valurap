0c3eda39cce62260a4259f5c60fefa4d0e0b4cb5
Vsevolod Lobko <seva@sevik.org>
2019-03-23 19:04:28 +0200

    fix example

--- zcad/frame.py
+++ zcad/frame.py
@@ -13,21 +13,21 @@ for (x, y) in [[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, 
     c1 = Connector([0, 0, 0], [0, 0, -1], top)
 
     a = base_long.place(pose={"top, front": c1})
-    shapes.extend(a.shapes())
+    shapes.extend(a.shapes().values())
 
     c2 = a.get_connector("bottom, front, left")
     new_pos = c2.position + c2.top * 10
     c3 = c2.replace(position=new_pos)
 
     b = base_short.place(pose={"top": c3}, config={"color": Color(0.1, 0.5, 0)})
-    shapes.extend(b.shapes())
+    shapes.extend(b.shapes().values())
 
     c4 = b.get_connector("bottom, left")
     new_pos = c4.position + c4.top * 10
     c5 = c4.replace(position=new_pos)
 
     c = base_short.place(pose={"top": c5})
-    shapes.extend(c.shapes())
+    shapes.extend(c.shapes().values())
 
 
 display(box(200, 200, 1).translate(-100, -100, -1), Color(0.5, 0.5, 0.5))
