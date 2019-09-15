13c5edc5af801e60650e073ce147e757a4916893
Vsevolod Lobko <seva@sevik.org>
2019-09-15 21:24:02 +0300

    right pars on one plate

--- freecad/print_fixture.py
+++ freecad/print_fixture.py
@@ -14,12 +14,51 @@ if not in_gui:
     App.newDocument("print")
     App.setActiveDocument("print")
 
+pp = []
+
 workdir = os.path.dirname(__file__)
+part = "left_y_idler_plate"
+Part.insert(f'{workdir}/{part}.brep', App.ActiveDocument.Name)
+p = App.ActiveDocument.getObject(part)
+if p.ViewObject:
+    p.ViewObject.Visibility = False
+
+bbox = p.Shape.BoundBox
+plc = App.Placement(
+    App.Vector(
+        -(bbox.XMin + bbox.XMax)/2,
+        -bbox.YMin,
+        -(bbox.ZMin + bbox.ZMax)/2,
+        ),
+    App.Rotation(App.Vector(0,0,1),0)
+)
+plc = App.Placement(
+    App.Vector(0, 0, 0),
+    App.Rotation(App.Vector(1,0,0),90)
+).multiply(plc)
+p.Placement = plc
+
+right_part = 'right' + part[4:]
+rp = App.ActiveDocument.addObject("Part::Mirroring", right_part)
+rp.Source = p
+rp.Label = right_part
+rp.Normal = (1, 0, 0)
+rp.Base = (0, 0, 0)
+plc = App.Placement(
+    App.Vector(0, 40, 0),
+    App.Rotation(App.Vector(1,0,0),0)
+)
+rp.Placement = plc
+if rp.ViewObject:
+    rp.ViewObject.Visibility = True
+
+pp.append(rp)
+
 part = "left_y_motor_plate"
 Part.insert(f'{workdir}/{part}.brep', App.ActiveDocument.Name)
 p = App.ActiveDocument.getObject(part)
 if p.ViewObject:
-    p.ViewObject.Visibility = True
+    p.ViewObject.Visibility = False
 
 bbox = p.Shape.BoundBox
 plc = App.Placement(
@@ -34,7 +73,25 @@ plc = App.Placement(
     App.Vector(0, 0, 0),
     App.Rotation(App.Vector(1,0,0),-90)
 ).multiply(plc)
+
 p.Placement = plc
+
+right_part = 'right' + part[4:]
+rp = App.ActiveDocument.addObject("Part::Mirroring", right_part)
+rp.Source = p
+rp.Label = right_part
+rp.Normal = (1, 0, 0)
+rp.Base = (0, 0, 0)
+plc = App.Placement(
+    App.Vector(0, -40, 0),
+    App.Rotation(App.Vector(1,0,0),0)
+)
+rp.Placement = plc
+
+if rp.ViewObject:
+    rp.ViewObject.Visibility = True
+
+pp.append(rp)
 
 heated_plate = App.ActiveDocument.addObject("Part::Box", "HeatedPlate_base")
 heated_plate.Length = "200 mm"
@@ -51,4 +108,4 @@ if heated_plate.ViewObject:
 print(heated_plate.Shape.BoundBox)
 
 import Mesh
-Mesh.export([p],f"{workdir}/{part}.stl")
+Mesh.export(pp,f"{workdir}/{right_part}.stl")
