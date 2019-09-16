09dbe62e72150ac0823258ba54f021fc75b6d6d6
Vsevolod Lobko <seva@sevik.org>
2019-09-16 09:14:56 +0300

    Y carriage plates

--- freecad/print_fixture.py
+++ freecad/print_fixture.py
@@ -17,81 +17,38 @@ if not in_gui:
 pp = []
 
 workdir = os.path.dirname(__file__)
-part = "left_y_idler_plate"
+part = "left_y_carriage_plate"
 Part.insert(f'{workdir}/{part}.brep', App.ActiveDocument.Name)
 p = App.ActiveDocument.getObject(part)
 if p.ViewObject:
-    p.ViewObject.Visibility = False
+    p.ViewObject.Visibility = True
 
 bbox = p.Shape.BoundBox
 plc = App.Placement(
     App.Vector(
         -(bbox.XMin + bbox.XMax)/2,
-        -bbox.YMin,
-        -(bbox.ZMin + bbox.ZMax)/2,
+        -(bbox.YMin + bbox.YMax)/2,
+        -bbox.ZMax,
         ),
     App.Rotation(App.Vector(0,0,1),0)
 )
-plc = App.Placement(
-    App.Vector(0, 0, 0),
-    App.Rotation(App.Vector(1,0,0),90)
-).multiply(plc)
-p.Placement = plc
-
-right_part = 'right' + part[4:]
-rp = App.ActiveDocument.addObject("Part::Mirroring", right_part)
-rp.Source = p
-rp.Label = right_part
-rp.Normal = (1, 0, 0)
-rp.Base = (0, 0, 0)
-plc = App.Placement(
-    App.Vector(0, 40, 0),
-    App.Rotation(App.Vector(1,0,0),0)
-)
-rp.Placement = plc
-if rp.ViewObject:
-    rp.ViewObject.Visibility = True
-
-pp.append(rp)
-
-part = "left_y_motor_plate"
-Part.insert(f'{workdir}/{part}.brep', App.ActiveDocument.Name)
-p = App.ActiveDocument.getObject(part)
-if p.ViewObject:
-    p.ViewObject.Visibility = False
-
-bbox = p.Shape.BoundBox
-plc = App.Placement(
-    App.Vector(
-        -(bbox.XMin + bbox.XMax)/2,
-        -bbox.YMax,
-        -(bbox.ZMin + bbox.ZMax)/2,
-        ),
-    App.Rotation(App.Vector(0,0,1),0)
-)
-plc = App.Placement(
-    App.Vector(0, 0, 0),
-    App.Rotation(App.Vector(1,0,0),-90)
-).multiply(plc)
-
-p.Placement = plc
-
-right_part = 'right' + part[4:]
-rp = App.ActiveDocument.addObject("Part::Mirroring", right_part)
-rp.Source = p
-rp.Label = right_part
-rp.Normal = (1, 0, 0)
-rp.Base = (0, 0, 0)
 plc = App.Placement(
     App.Vector(0, -40, 0),
+    App.Rotation(App.Vector(1,0,0),180)
+).multiply(plc)
+p.Placement = plc
+
+pp.append(p)
+
+rycp = App.ActiveDocument.addObject('PartDesign::FeatureBase','rycp_base')
+rycp.BaseFeature = p
+plc = App.Placement(
+    App.Vector(0, 80, 0),
     App.Rotation(App.Vector(1,0,0),0)
-)
-rp.Placement = plc
+).multiply(p.Placement)
+rycp.Placement = plc
 
-if rp.ViewObject:
-    rp.ViewObject.Visibility = True
-
-pp.append(rp)
+pp.append(rycp)
 
 heated_plate = App.ActiveDocument.addObject("Part::Box", "HeatedPlate_base")
 heated_plate.Length = "200 mm"
@@ -108,4 +65,4 @@ if heated_plate.ViewObject:
 print(heated_plate.Shape.BoundBox)
 
 import Mesh
-Mesh.export(pp,f"{workdir}/{right_part}.stl")
+Mesh.export(pp,f"{workdir}/{part}.stl")
