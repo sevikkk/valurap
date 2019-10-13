272bd969db0295aa9fcedc56940a5a3430e34672
Vsevolod Lobko <seva@sevik.org>
2019-10-13 12:30:54 +0300

    current state of host_soft

---
+++ host_soft/valurap/valurap/scripts/server.py
@@ -0,0 +1,23 @@
+import asyncio
+
+import aiomonitor
+from aiohttp import web
+
+
+async def simple(request):
+    loop = request.app.loop
+    print('Start sleeping')
+    await asyncio.sleep(100, loop=loop)
+    return web.Response(text='Simple answer')
+
+
+async def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_get('/simple', simple)
+    return app
+
+loop = asyncio.get_event_loop()
+app = loop.run_until_complete(init(loop))
+
+with aiomonitor.start_monitor(loop=loop):
+    web.run_app(app, port=8080, host='0.0.0.0')
