4a80248850b1bc84089ce48ad7833adfc7c67bd0
Vsevolod Lobko <seva@sevik.org>
2019-10-14 15:20:19 +0300

    uart first steps

--- stm_soft/stm_main/Src/task_blink.c
+++ stm_soft/stm_main/Src/task_blink.c
@@ -1,14 +1,42 @@
 #include "main.h"
 #include "cmsis_os.h"
+#include <stdio.h>
+
+extern UART_HandleTypeDef huart2;
+
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
 
 void StartDebugBlink(void const * argument)
 {
+  int i = 0;
   for(;;)
   {
     HAL_GPIO_WritePin(DEBUG_LED_GPIO_Port, DEBUG_LED_Pin, GPIO_PIN_RESET);
-    osDelay(100);
+    osDelay(50);
     HAL_GPIO_WritePin(DEBUG_LED_GPIO_Port, DEBUG_LED_Pin, GPIO_PIN_SET);
-    osDelay(500);
+    osDelay(450);
+    printf("%d. ", i);
+    fflush(0);
+    i++;
+    /* HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, 0xFFFF); */
   }
 }
 
+void uart_putc(char ch) {
+      HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, 0xFFFF);
+}
+
+int _write_r (struct _reent *r, int file, char * ptr, int len)
+{
+  if (file == 1 || file == 2) {
+     int index;
+     for(index=0; index<len; index++) {
+          if (ptr[index] == '\n')
+          {
+            uart_putc('\r');
+          }
+          uart_putc(ptr[index]);
+     }
+  }
+  return len;
+}
