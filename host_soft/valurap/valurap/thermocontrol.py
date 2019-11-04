8cbe41d590b7a8f3d4e517b21e18da8ece90c245
Vsevolod Lobko <seva@sevik.org>
2019-11-04 11:42:13 +0200

    thermocontrol s3g commands

---
+++ host_soft/valurap/valurap/thermocontrol.py
@@ -0,0 +1,59 @@
+from collections import namedtuple
+
+import struct
+
+from .s3g import S3GPortBase
+
+class ThermoC(S3GPortBase):
+    default_port = '/dev/ttyS2'
+
+    def S3G_PING(self, value=0, cmd_id=None):
+        """
+        Send strobe
+        """
+        payload = struct.pack('<BI', 0, value)
+
+        reply = self.send_and_wait_reply(payload, cmd_id)
+        if reply[0] != 0x81:
+            raise RuntimeError("Unexpected reply code")
+        value = struct.unpack("<I", reply[1:5])[0]
+        return value
+
+    t_query_reply = namedtuple(
+        "t_query_reply",
+        "k_type adc1 adc2 adc3 ext1 ext2 ext3 target1 target2 target3")
+
+    def S3G_QUERY(self, cmd_id=None):
+        """
+        Send strobe
+        """
+        payload = struct.pack('<B', 1)
+
+        reply = self.send_and_wait_reply(payload, cmd_id)
+        if reply[0] != 0x81:
+            raise RuntimeError("Unexpected reply code")
+        value = self.t_query_reply(*struct.unpack("<HHHHHHHHHH", reply[1:]))
+        return value
+
+    def S3G_SET_PID_TARGET(self, channel, target, cmd_id=None):
+        """
+        Send strobe
+        """
+        payload = struct.pack('<BBH', 2, channel, target)
+
+        reply = self.send_and_wait_reply(payload, cmd_id)
+        if reply[0] != 0x81:
+            raise RuntimeError("Unexpected reply code")
+        return
+
+    def S3G_SET_PID_PARAMS(self, channel, k_p, k_i, cmd_id=None):
+        """
+        Send strobe
+        """
+        payload = struct.pack('<BBHH', 3, channel, k_p, k_i)
+
+        reply = self.send_and_wait_reply(payload, cmd_id)
+        if reply[0] != 0x81:
+            raise RuntimeError("Unexpected reply code")
+        return
+
