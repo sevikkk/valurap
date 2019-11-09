6a8ddfa227ee179de947baf15bc81ee7bf6b143d
Vsevolod Lobko <seva@sevik.org>
2019-11-09 10:44:22 +0200

    calculate params for thermistors, calibrate heatbed

--- host_soft/valurap/valurap/scripts/pid_calibrate.py
+++ host_soft/valurap/valurap/scripts/pid_calibrate.py
@@ -6,16 +6,17 @@ tc = ThermoC()
 #print(q)
 #q = tc.S3G_SET_PID_TARGET(1, 500)
 #print(q)
-q = tc.S3G_SET_PID_PARAMS(1, 20000, 200)
+q = tc.S3G_SET_PID_PARAMS(3, 20000, 200)
 print(q)
 
-targets = list(range(200, 255, 5)) + list(range(245,20,-5)) + list(range(30,260,10)) + list(range(240,20,-10))
+#targets = list(range(30, 255, 30)) + list(range(245,20,-30)) + list(range(30,260,10)) + list(range(240,20,-10))
+targets = list(range(30, 130, 20)) + list(range(110,30,-20))
 print(targets)
 for target in targets:
-    q = tc.S3G_SET_PID_TARGET(1, target)
+    q = tc.S3G_SET_PID_TARGET(3, target)
     print(q)
-    for i in range(180):
+    for i in range(300):
         q = tc.S3G_QUERY()
         print(q)
-        open("tc.txt", "a").write("%d %d %d %d %d\n" % (time.time(), q.k_type, q.adc1, q.target1, q.ext1))
+        open("tc.txt", "a").write("%d %d %d %d %d\n" % (time.time(), q.k_type, q.adc3, q.target3, q.ext3))
         time.sleep(1)
