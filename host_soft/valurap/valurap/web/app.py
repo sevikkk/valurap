50bfd135f8d95e394a25a8d14f2ff52472d901a2
Vsevolod Lobko <seva@sevik.org>
2019-12-29 00:00:26 +0200

    response

--- host_soft/valurap/valurap/web/app.py
+++ host_soft/valurap/valurap/web/app.py
@@ -5,20 +5,29 @@ from flask_socketio import SocketIO, join_room, emit, send
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+current_state = {
+    "X": 0,
+    "Y": 0,
+    "Z": 0
+}
+
 @app.route('/')
 def index():
     """Serve the index HTML"""
     return render_template('index.html')
 
-@socketio.on('hello')
-def on_hello(data):
-    """Create a game lobby"""
-    print("hello:", data)
+@socketio.on('send_command')
+def on_send_command(data):
+    print("got_command:", data)
+    if data == "up":
+        current_state["Y"] += 1
+    elif data == "down":
+        current_state["Y"] -= 1
+    elif data == "left":
+        current_state["X"] -= 1
+    elif data == "right":
+        current_state["X"] += 1
 
-@socketio.on('create')
-def on_create(data):
-    """Create a game lobby"""
-    print("connected:", data)
-    emit('hello', {'bubu': 'bebe'})
+    emit('cur_state', current_state)
 
 socketio.run(app, host="0.0.0.0")
