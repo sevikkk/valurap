98bee5880e61a120b91b60a4c8888831b11e45fa
Vsevolod Lobko <seva@sevik.org>
2019-12-28 23:47:54 +0200

    socket.io send

--- host_soft/valurap/valurap/web/valurap-ui/src/App.vue
+++ host_soft/valurap/valurap/web/valurap-ui/src/App.vue
@@ -1,28 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="col-sm-12">
+      Valurap Control Panel
+    </div>
+    <div class="row">
+      <div class="col-sm-8"/>
+      <div class="col-sm-3">
+        <div class="row">
+          <div class="col-sm-12">
+            <b-button class="my-1" @click="sendCommand('up')">^</b-button>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-12">
+            <b-button class="my-1 mx-4" @click="sendCommand('left')">&lt;</b-button>
+            <b-button class="my-1 mx-4" @click="sendCommand('right')">&gt;</b-button>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-12">
+            <b-button class="my-1" @click="sendCommand('down')">v</b-button>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-1"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    components: {},
+    methods: {
+    sendCommand: function (cmd) {
+      this.$socket.emit('send_command', cmd)
+    }
   }
-}
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- host_soft/valurap/valurap/web/valurap-ui/src/main.js
+++ host_soft/valurap/valurap/web/valurap-ui/src/main.js
@@ -4,8 +4,19 @@ import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
 import store from './store'
+import VueSocketIO from 'vue-socket.io';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(new VueSocketIO({
+    debug: true,
+    connection: `//${window.location.host}`,
+    vuex: {
+        store,
+        actionPrefix: 'SOCKET_',
+        mutationPrefix: 'SOCKET_'
+    },
+    options: { path: "/socket.io/" } //Optional options
+}))
 
 new Vue({
   store,
