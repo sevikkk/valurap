57f84883708746b2805c55e5de12503ef060bb58
Vsevolod Lobko <seva@sevik.org>
2019-12-29 20:33:14 +0200

    async mode emu

--- host_soft/valurap/valurap/web/app.py
+++ host_soft/valurap/valurap/web/app.py
@@ -1,3 +1,5 @@
+import time
+
 from flask import Flask, render_template
 from flask_socketio import SocketIO, join_room, emit, send
 
@@ -8,7 +10,13 @@ socketio = SocketIO(app)
 current_state = {
     "X": 0,
     "Y": 0,
-    "Z": 0
+    "Z": 0,
+}
+
+current_speeds = {
+    "X": 0,
+    "Y": 0,
+    "Z": 0,
 }
 
 @app.route('/')
@@ -27,7 +35,43 @@ def on_send_command(data):
         current_state["X"] -= 1
     elif data == "right":
         current_state["X"] += 1
+    elif data == "start-up":
+        current_speeds["Y"] = 1
+    elif data == "start-down":
+        current_speeds["Y"] = 1
+    elif data == "start-left":
+        current_speeds["X"] = 1
+    elif data == "start-right":
+        current_speeds["X"] = 1
+    elif data == "stop-up":
+        current_speeds["Y"] = 0
+    elif data == "stop-down":
+        current_speeds["Y"] = 0
+    elif data == "stop-left":
+        current_speeds["X"] = 0
+    elif data == "stop-right":
+        current_speeds["X"] = 0
 
     emit('cur_state', current_state, broadcast=True)
+
+import threading
+
+class Incrementer(threading.Thread):
+    def run(self):
+        while True:
+            need_update = False
+            for k,v in current_speeds.items():
+                if v != 0:
+                    current_state[k] += v
+                    need_update = True
+
+            if need_update:
+                socketio.emit('cur_state', current_state, broadcast=True)
+
+            time.sleep(0.2)
+
+t = Incrementer()
+t.setDaemon(True)
+t.start()
 
 socketio.run(app, host="0.0.0.0")
