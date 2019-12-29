b5cdf0cd772029166b3e558e53dae5b9d369ff4b
Vsevolod Lobko <seva@sevik.org>
2019-12-29 20:00:18 +0200

    design

--- host_soft/valurap/valurap/web/valurap-ui/src/App.vue
+++ host_soft/valurap/valurap/web/valurap-ui/src/App.vue
@@ -4,37 +4,45 @@
       Valurap Control Panel
     </div>
     <div class="row">
-      <div class="col-sm-3"/>
-      <div class="col-sm-2">
+      <div class="col-sm-1"/>
+      <div class="col-sm-3">
         <div class="row">
-          <div v-model="show" class="d-inline">X: {{$store.state.cur_x}}</div>
+          <div class="d-inline">X: {{$store.state.cur_x}}</div>
         </div>
         <div class="row">
-          <div v-model="show" class="d-inline">Y: {{$store.state.cur_y}}</div>
+          <div class="d-inline">Y: {{$store.state.cur_y}}</div>
         </div>
         <div class="row">
-          <div v-model="show" class="d-inline">Z: {{$store.state.cur_z}}</div>
-        </div>
-      </div>
-      <div class="col-sm-2"/>
-      <div class="col-sm-4">
-        <div class="row">
-          <div class="col-sm-12">
-            <Button cmd="up" class="my-3" comment="^"/>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-sm-12">
-            <Button cmd="left" class="mx-5" comment="&lt;"/>
-            <Button cmd="right" class="mx-5" comment="&gt;"/>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-sm-12">
-            <Button cmd="down" class="my-3" comment="v"/>
-          </div>
+          <div class="d-inline">Z: {{$store.state.cur_z}}</div>
         </div>
       </div>
+      <div class="col-sm-4"/>
+      <div class="my-button-box">
+        <div class="row">
+          <div class="col-sm-4"/>
+          <div class="col-sm-4 p-2">
+            <Button cmd="up" class="py-4 btn-block" comment="^"/>
+          </div>
+          <div class="col-sm-4"/>
+        </div>
+        <div class="row">
+          <div class="col-sm-4 p-2">
+            <Button cmd="left" class="py-4 btn-block" comment="&lt;"/>
+          </div>
+          <div class="col-sm-4"/>
+          <div class="col-sm-4 p-2">
+            <Button cmd="right" class="py-4 btn-block" comment="&gt;"/>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-4"/>
+          <div class="col-sm-4 p-2">
+            <Button cmd="down" class="py-4 btn-block" comment="v"/>
+          </div>
+          <div class="col-sm-4"/>
+        </div>
+      </div>
+      <div class="col-sm-1"/>
     </div>
   </div>
 </template>
@@ -63,5 +71,11 @@
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
+  }
+  .my-button-box {
+     min-width: 20em;
+     max-width: 20em;
+    min-height: 30em;
+    max-height: 30em;
   }
 </style>
