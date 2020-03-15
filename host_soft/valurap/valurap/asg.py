799585b6dfca33bf2455a105dc5f4637415a88f3
Vsevolod Lobko <seva@sevik.org>
2020-03-15 19:44:38 +0200

    to/from tuple conversion so it can be safely pickled

--- host_soft/valurap/valurap/asg.py
+++ host_soft/valurap/valurap/asg.py
@@ -16,6 +16,62 @@ class ProfileSegment(object):
                 s.append('{}={}'.format(a, v))
         return " ".join(s) + '>'
 
+    def to_tuple(self):
+        ret = []
+        if self.jj != 0:
+            ijj = self.jj
+            if ijj is not None:
+                ijj = int(ijj)
+            ret.append(ijj)
+
+        if ret or (self.j != 0):
+            ij = self.j
+            if ij is not None:
+                ij = int(ij)
+            ret.append(ij)
+
+        if ret or (self.target_v is not None):
+            itv = self.target_v
+            if itv is not None:
+                itv = int(itv)
+
+            ret.append(itv)
+
+        if ret or (self.a != 0):
+            ia = self.a
+            if ia is not None:
+                ia = int(ia)
+            ret.append(ia)
+
+        if ret or (self.v is not None):
+            iv = self.v
+            if iv is not None:
+                iv = int(iv)
+            ret.append(iv)
+
+        ix = self.x
+        if ix is not None:
+            ix = int(ix)
+        ret.append(ix)
+        ret.append(self.apg.name)
+
+        ret = tuple(reversed(ret))
+
+        return(ret)
+
+    @classmethod
+    def from_tuple(cls, tup, apgs):
+        kws = {}
+        l = list(tup)
+        apg_name = l.pop(0)
+        kws["apg"] = apgs[apg_name]
+
+        for fld in ["x", "v", "a", "target_v", "j", "jj"]:
+            if not l:
+                break
+            kws[fld] = l.pop(0)
+
+        return cls(**kws)
 
 class PathSegment(object):
     def __init__(self, axis, dx, base_v):
