cac705c52e5a1c8c43a2716c29be8a2f0a479148
Vsevolod Lobko <seva@sevik.org>
2020-04-05 14:54:11 +0300

    do not use analog current adjustments, set work and hold current

--- host_soft/valurap/valurap/spi.py
+++ host_soft/valurap/valurap/spi.py
@@ -7,6 +7,6 @@ class SPIPort(object):
         self.dev.open(1, 0)
 
     def setup_tmc2130(self):
-        self.dev.xfer2([0x80, 0, 0, 0, 1] * 12)
-        self.dev.xfer2([0x90, 0, 0, 16, 16] * 12)
+        self.dev.xfer2([0x80, 0, 0, 0, 0] * 12)
+        self.dev.xfer2([0x90, 0, 0, 16, 8] * 12)
         self.dev.xfer2([0x6c + 0x80, 0x04, 0x00, 0x80, 0x08] * 12)
