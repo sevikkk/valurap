f69bde168e2811e8c6038c109a875548de8545a6
Vsevolod Lobko <seva@sevik.org>
2020-07-26 22:31:01 +0300

    speed up profiles

--- host_soft/valurap/nbs/gen_layers.py
+++ host_soft/valurap/nbs/gen_layers.py
@@ -8,6 +8,21 @@ planner.emu_in_loop = True
 planner.delta_e_err = 10
 planner.delta_ve_err = 10
 planner.delta_err = 3
+planner.skip_plato_len = 0.05
 
-planner.gen_layers(sys.argv[1])
+if 0:
+    speed_k = 1.5
+    planner.max_xa = 3000
+    planner.max_ya = 3000
+
+if 1:
+    speed_k = 2.0
+    planner.max_xa = 3000
+    planner.max_ya = 3000
+    planner.delta_err = 3
+    planner.delta_v_err = 3
+    planner.delta_e_err = 30
+    planner.delta_ve_err = 20
+
+planner.gen_layers(sys.argv[1], speed_k=speed_k)
 
