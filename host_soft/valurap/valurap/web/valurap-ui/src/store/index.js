47cede97a210b664616f2cc9c57f25b31fe795c2
Vsevolod Lobko <seva@sevik.org>
2020-08-02 18:09:19 +0300

    slightly more logging

--- host_soft/valurap/valurap/web/valurap-ui/src/store/index.js
+++ host_soft/valurap/valurap/web/valurap-ui/src/store/index.js
@@ -23,7 +23,31 @@ export default new Vuex.Store({
       console.log("bubu1");
     },
     SOCKET_connect(message) {
-      console.log("bubu2");
+      console.log("socket connect");
+    },
+    SOCKET_error(message) {
+      console.log("socket error");
+    },
+    SOCKET_disconnect(message) {
+      console.log("socket disconnect");
+    },
+    SOCKET_connect_error(message) {
+      console.log("socket connect_error");
+    },
+    SOCKET_connect_timeout(message) {
+      console.log("socket connect_timeout");
+    },
+    SOCKET_reconnect_error(message) {
+      console.log("socket reconnect_error");
+    },
+    SOCKET_reconnect_failed(message) {
+      console.log("socket reconnect_failed");
+    },
+    SOCKET_ping(message) {
+      console.log("socket ping");
+    },
+    SOCKET_pong(message) {
+      console.log("socket pong");
     },
   },
   modules: {
