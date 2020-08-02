be1d3f5860365a360195b784af34c96c375df14e
Vsevolod Lobko <seva@sevik.org>
2020-08-02 18:39:41 +0300

    typo

--- host_soft/valurap/nbs/gen_layers.py
+++ host_soft/valurap/nbs/gen_layers.py
@@ -9,13 +9,14 @@ planner.delta_e_err = 10
 planner.delta_ve_err = 10
 planner.delta_err = 3
 planner.skip_plato_len = 0.05
+speed_k = 1.0
 
 if 0:
     speed_k = 1.5
     planner.max_xa = 3000
     planner.max_ya = 3000
 
-if 1:
+if 0:
     speed_k = 2.0
     planner.max_xa = 3000
     planner.max_ya = 3000
