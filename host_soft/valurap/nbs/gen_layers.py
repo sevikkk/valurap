ff7953e5dd55f72880e7051d61babfcbeada6c84
Vsevolod Lobko <seva@sevik.org>
2020-09-27 16:20:53 +0300

    adjust parameters

--- host_soft/valurap/nbs/gen_layers.py
+++ host_soft/valurap/nbs/gen_layers.py
@@ -5,11 +5,14 @@ planner = pp2.PathPlanner()
 planner.max_seg = 10.0
 planner.min_seg = 0.2
 planner.emu_in_loop = True
-planner.delta_e_err = 10
-planner.delta_ve_err = 10
+planner.delta_e_err = 30
+planner.delta_ve_err = 80
 planner.delta_err = 3
 planner.skip_plato_len = 0.05
 speed_k = 1.0
+planner.max_xa = 3000
+planner.max_ya = 3000
+planner.espeed_by_de = True
 
 if 0:
     speed_k = 1.5
@@ -20,10 +23,11 @@ if 0:
     speed_k = 2.0
     planner.max_xa = 3000
     planner.max_ya = 3000
+    planner.max_ea = 3000
     planner.delta_err = 3
     planner.delta_v_err = 3
     planner.delta_e_err = 30
-    planner.delta_ve_err = 20
+    planner.delta_ve_err = 10
 
 planner.gen_layers(sys.argv[1], speed_k=speed_k)
 
