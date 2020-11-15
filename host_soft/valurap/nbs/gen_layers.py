6e6e5977da89052a133bc7b6b4db62f7b7401eec
Vsevolod Lobko <seva@sevik.org>
2020-11-15 18:07:28 +0200

    current state

--- host_soft/valurap/nbs/gen_layers.py
+++ host_soft/valurap/nbs/gen_layers.py
@@ -3,16 +3,23 @@ import sys
 
 planner = pp2.PathPlanner()
 planner.max_seg = 10.0
-planner.min_seg = 0.2
+planner.min_seg = 0.1
 planner.emu_in_loop = True
-planner.delta_e_err = 30
+planner.delta_e_err = 80
 planner.delta_ve_err = 80
 planner.delta_err = 3
-planner.skip_plato_len = 0.05
+planner.skip_plato_len = 0.03
 speed_k = 1.0
 planner.max_xa = 3000
 planner.max_ya = 3000
 #planner.espeed_by_de = True
+
+if 1:
+    speed_k = 1.5
+    planner.min_seg = 0.05
+    planner.skip_plato_len = 0.01
+    planner.delta_err = 5
+    planner.delta_v_err = 5
 
 if 0:
     speed_k = 1.5
