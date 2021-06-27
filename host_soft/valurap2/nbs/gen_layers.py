07e80402018bdfee8e6aee547a47e6f4f60043bc
Vsevolod Lobko <seva@sevik.org>
2021-06-27 21:35:35 +0300

    turbo profile

--- host_soft/valurap2/nbs/gen_layers.py
+++ host_soft/valurap2/nbs/gen_layers.py
@@ -11,9 +11,9 @@ if 1:
     pp.delta_ve_err = 1000
     pp.max_ea = 10000
 else:
-    speed_k = 10.0
-    pp.delta_e_err = 100
-    pp.delta_ve_err = 100
+    speed_k = 2.5
     pp.max_ea = 20000
+    pp.max_xa = 3000
+    pp.max_ya = 3000
 
 pp.gen_layers(sys.argv[1], speed_k=speed_k)
